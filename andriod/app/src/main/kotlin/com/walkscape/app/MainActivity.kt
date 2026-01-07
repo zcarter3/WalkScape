8c3baeb18c38b3020f5c5a9113de7dfaf2ec0b81
Zae <zcarter3@my.tnstate.edu>
2026-01-07 15:42:20 +0000

    Clean up project configuration
    - Remove conflicting misspelled 'andriod' folder
    - Remove old 'Web' folder to avoid conflicts with 'web' folder
    - Remove unnecessary 'web' package from pubspec.yaml dependencies
    - Flutter handles web support automatically, no need for web package

--- andriod/app/src/main/kotlin/com/walkscape/app/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.walkscape.app
-
-import io.flutter.embedding.android.FlutterFragmentActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
-
-class MainActivity: FlutterFragmentActivity() {
-}
