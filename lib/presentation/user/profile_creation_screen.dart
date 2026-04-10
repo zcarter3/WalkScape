f4ed71aa685428b74030d1c9d5747ebf6703416a
Zae <zcarter3@my.tnstate.edu>
2026-04-10 02:34:05 +0000

    bugs

--- lib/presentation/user/profile_creation_screen.dart
+++ lib/presentation/user/profile_creation_screen.dart
@@ -1,1 +1,15 @@
-// ...existing code...
+import 'package:flutter/material.dart';
+
+class ProfileCreationScreen extends StatelessWidget {
+	const ProfileCreationScreen({super.key});
+
+	@override
+	Widget build(BuildContext context) {
+		return Scaffold(
+			appBar: AppBar(title: const Text('Create Profile')),
+			body: const Center(
+				child: Text('Profile creation coming soon!'),
+			),
+		);
+	}
+}
